94bef991477a23feb384111ac9c72b363165ce93
Saac Sornchai <sornchai.la@ku.th>
2022-05-25 13:29:57 +0700

    sample index.php

--- web/public/index.php
+++ web/public/index.php
@@ -1,15 +1,19 @@
 <?php
 
 include '../app/vendor/autoload.php';
-$foo = new App\Acme\Foo();
+$foo = new App\Foo();
+$date = new DateTime();
 
 ?><!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Docker <?php echo $foo->getName(); ?></title>
-    </head>
-    <body>
-        <h1>Docker <?php echo $foo->getName(); ?></h1>
-    </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $foo->getName(); ?></title>
+</head>
+<body>
+    <h1>Docker <?= $foo->getName() ?></h1>
+    <h2><?= $date->format('Y-m-d\TH:i:s.u'); ?></h2>
+</body>
 </html>
